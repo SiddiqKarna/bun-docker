1d55b3087fecbfd95c898e125acc9e7cdcb11d88
SiddiqKarna <147962236+SiddiqKarna@users.noreply.github.com>
2023-10-15 09:45:55 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,9 +4,12 @@ const NODE_ENV = process.env.NODE_ENV ?? "development";
 const server = Bun.serve({
   port: PORT,
   fetch() {
-    process.exit(1);
     return new Response("Welcome to Bun!");
   },
 });
 
 console.log(`[${NODE_ENV}] Listening on port ${server.port}`);
+
+setInterval(() => {
+    process.exit(1);
+}, 10 * 1000)
